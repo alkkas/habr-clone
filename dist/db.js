491199af28f7e9a1323d7e63af0a08c1db10fc92
Egor Chernykh <90968281+alkkas@users.noreply.github.com>
2023-02-27 19:30:58 +0300

    deleting dist folder

--- dist/db.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-console.log(process.env.DATABASE_URL);
-const sequelize = new sequelize_1.Sequelize((_a = process.env.DATABASE_URL) !== null && _a !== void 0 ? _a : "");
-exports.default = sequelize;
